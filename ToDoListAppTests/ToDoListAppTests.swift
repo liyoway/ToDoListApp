a65ceec86cfe1d727b458bd8de6f457e47c92370
liyoway <liyoway@gmail.com>
2024-10-01 03:01:19 +0900

    Initial Commit

---
+++ ToDoListAppTests/ToDoListAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppTests.swift
+//  ToDoListAppTests
+//
+//  Created by yoway Li on 2024/10/1.
+//
+
+import Testing
+@testable import ToDoListApp
+
+struct ToDoListAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
